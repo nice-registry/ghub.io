a2680450f829a79ffcb09107b733eb54787e643f
Zeke Sikelianos <zeke@sikelianos.com>
2017-11-17 19:46:26 -0800

    mvp with tests

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const port = Number(process.env.PORT) || 5000
+const repos = require('all-the-package-repos')
+const app = express()
+
+app.get('/:name', (req, res, next) => {
+  const name = req.params.name
+  const repo = repos[name] || `https://npmjs.com/package/${name}`
+  res.redirect(repo)
+})
+
+if (!module.parent) {
+  app.listen(port, () => {
+    console.log(`app running on ${port}`)
+  })
+}
+
+module.exports = app
---
+++ test/server.js
@@ -0,0 +1,19 @@
+const {describe, it} = require('mocha')
+const supertest = require('supertest')
+const chai = require('chai')
+chai.should()
+const app = require('../server.js')
+
+describe('package.land', () => {
+  it('redirects known package names to GitHub', async () => {
+    const res = await supertest(app).get(`/express`)
+    res.statusCode.should.equal(302)
+    res.headers.location.should.equal('https://github.com/expressjs/express')
+  })
+
+  it('redirects unknown package names to npm', async () => {
+    const res = await supertest(app).get(`/not-gonna-be-a-package-name`)
+    res.statusCode.should.equal(302)
+    res.headers.location.should.equal('https://npmjs.com/package/not-gonna-be-a-package-name')
+  })
+})
