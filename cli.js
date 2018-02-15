b7a4c1253255e2f8c7363ceaafd27e9588dc77a8
Apidcloud <luisapidcloud@hotmail.com>
2018-02-15 00:43:38 +0000

    feat: try to use current repo name when there are no args

--- cli.js
+++ cli.js
@@ -4,19 +4,26 @@ const open = require('open')
 const readline = require('readline')
 const dedent = require('dedent')
 const getRepo = require('./lib/get-repo')
+const getCurrentRepo = require('./lib/get-current-repo')
 
 if (process.stdin.isTTY) {
   // Read from arguments
   const names = process.argv.slice(2)
   if (!names.length) {
-    console.log(dedent`To use ghub, specify one or many package names.
+    // Get current repo, if any
+    const currentRepo = getCurrentRepo()
+    // Fallback to usage example
+    if (currentRepo === '') {
+      console.log(dedent`To use ghub, specify one or many package names.
       Examples:
 
       ghub express
       ghub choo chai chalk`)
 
-    // exit gracefully to avoid dumping a bunch of npm info
-    process.exit()
+      // Exit gracefully to avoid dumping a bunch of npm info
+      process.exit()
+    }
+    names.push(currentRepo)
   }
   names.forEach(name => open(getRepo(name)))
 } else {
